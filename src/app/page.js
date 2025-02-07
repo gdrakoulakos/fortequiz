276351a19a3afad9a9f3c4bea7369c82f6a34b24
gdrakoulakos <georgediopan@gmail.com>
2025-02-08 00:46:35 +0200

    Add the components Header, WelcomeBanner, CardQuiz, Footer on main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,18 @@
-import Link from "next/link";
-import cardQuizData from "../data/cardQuizData.json";
 import styles from "./page.module.css";
+import Header from "@/components/organisms/Header/Header";
+import Footer from "@/components/organisms/Footer/Footer";
+import CardQuiz from "@/components/molecules/CardQuiz/CardQuiz";
+import WelcomeBanner from "@/components/organisms/WelcomeBanner/WelcomeBanner";
 
 export default function Home() {
-  const headerList = [
-    { id: 1, name: "Theory", link: "/theory" },
-    { id: 2, name: "History", link: "/history" },
-    { id: 3, name: "Instruments", link: "/instruments" },
-    { id: 4, name: "Ear Testing", link: "/earTesting" },
-    { id: 5, name: "Modern", link: "/modern" },
-    { id: 6, name: "Trivia", link: "/trivia" },
-    { id: 7, name: "About Us", link: "/aboutUs" },
-  ];
-
   return (
     <div className={styles.page}>
-      <header className={styles.header}>
-        <nav>
-          <ul className={styles.navList}>
-            {headerList.map(({ id, link, name }) => (
-              <li key={id}>
-                <Link href={link}>{name}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </header>
+      <Header />
       <main className={styles.main}>
-        <div className={styles.welcomeBanner}>
-          <h1>Welcome to forteQuiz</h1>
-          <p1>Play some Quizzes and test your music skills</p1>
-        </div>
-        <div className={styles.allQuizContainer}>
-          <div className={styles.theoryQuizContainer}>
-            {cardQuizData.category.map((category) => (
-              <div key={category.id}>
-                <h2>{category.name}</h2>
-                <div className={styles.quizCard}>
-                  {category.quizzes.map((quiz) => (
-                    <div key={quiz.id}>
-                      <h3>{quiz.name}</h3>
-                      <p>{quiz.description}</p>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        <WelcomeBanner />
+        <CardQuiz />
       </main>
-      <footer className={styles.footer}>
-        <nav>
-          <ul className={styles.footerList}>
-            {headerList.map(({ id, link, name }) => (
-              <li key={id}>
-                <Link href={link}>{name}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </footer>
+      <Footer />
     </div>
   );
 }
