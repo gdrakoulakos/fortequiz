142ccf0d747903b6a0d19d501f918ee2417d2d30
gdrakoulakos <georgediopan@gmail.com>
2025-02-11 16:17:44 +0200

    Create Button Play

---
+++ src/components/atoms/ButtonPlay/ButtonPlay.js
@@ -0,0 +1,9 @@
+import styles from "../ButtonPlay/ButtonPlay.module.css"
+
+export default function ButtonPlay () {
+    return(
+        <div className={styles.buttonContainer}>
+            <button>Play Now</button>
+        </div>
+    )
+}
