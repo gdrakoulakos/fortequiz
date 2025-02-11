a5f405f25e84889666924ab3a84f4905a05ca47e
gdrakoulakos <georgediopan@gmail.com>
2025-02-11 20:28:52 +0200

    Change data source from mock to theoryQuestions.json

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -1,17 +1,20 @@
 import styles from "../pages/quiz.module.css";
+import theoryQuestions from "../data/theoryQuestions.json";
 
 export default function Quiz() {
+  const questionsData = theoryQuestions.questions[0];
+
   return (
     <div className={styles.quizContainer}>
-      <h1>Theory</h1>
-      <h2>Which note is after A?</h2>
+      <h1>{theoryQuestions.category}</h1>
+      <h2>{questionsData.title}</h2>
       <h1>🕒</h1>
-      <h2>3</h2>
+      <h2>{theoryQuestions.time}</h2>
       <div className={styles.quizAnswers}>
-        <h3>The note B is after A</h3>
-        <h3>The note C is after A</h3>
-        <h3>The note D is after A</h3>
-        <h3>The note E is after A</h3>
+        <h3>{questionsData.answer1}</h3>
+        <h3>{questionsData.answer2}</h3>
+        <h3>{questionsData.answer3}</h3>
+        <h3>{questionsData.answer4}</h3>
       </div>
     </div>
   );
