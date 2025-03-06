61c4fc6c8b493c1f256a7d1e0605c6266c4a268c
gdrakoulakos <georgediopan@gmail.com>
2025-03-06 11:46:11 +0200

    Change styling of questions results

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -1,5 +1,5 @@
 "use client";
-import quizStyles from "./quiz.module.css";
+import styles from "./quiz.module.css";
 import buttonStyles from "@/components/atoms/ButtonAnswer/ButtonAnswer.module.css"; // Import button styles
 import { useState } from "react";
 import ButtonAnswer from "@/components/atoms/ButtonAnswer/ButtonAnswer";
@@ -33,48 +33,52 @@ export default function Quiz() {
 
   return (
     selectedQuiz && (
-      <div className={quizStyles.quizContainer}>
-        <h1>{selectedQuiz.category}</h1>
-        <h2>{selectedQuiz.subcategory}</h2>
-        <h2>Σωστές Απαντήσεις: {totalCorrectAnswers}</h2>
-
-        {selectedQuiz?.questions?.map((question, questionIndex) => (
-          <div key={questionIndex} className={quizStyles.questionBlock}>
-            <h3>{`${question.id}. ${question.title}`}</h3>
-
-            {question.availableAnswers.map((answer, answerIndex) => (
-              <div
-                key={answerIndex}
-                className={quizStyles.quizAnswersContainer}
-              >
-                <span>
-                  <ButtonAnswer
-                    className={`${buttonStyles.answerButton} ${
-                      selectedAnswers[questionIndex]?.[answerIndex] ||
-                      (selectedAnswers[questionIndex]?.correctIndex === answer
-                        ? buttonStyles.correctAnswer
-                        : "")
-                    }`}
-                    onClick={() =>
-                      handleAnswer(
-                        answer,
-                        questionIndex,
-                        answerIndex,
-                        question.correctAnswer
-                      )
-                    }
-                    disabled={
-                      Object.keys(selectedAnswers[questionIndex] || {}).length >
-                      0
-                    }
-                  >
-                    {answer}
-                  </ButtonAnswer>
-                </span>
-              </div>
-            ))}
+      <div className={styles.quizContainer}>
+        <div className={styles.questionsInfo}>
+        <div className={styles.questionsInfoTop}>
+          <h2>{selectedQuiz.category}</h2>
+          <h3>{selectedQuiz.subcategory}</h3>
           </div>
-        ))}
+          <div>
+            Σκορ: {totalCorrectAnswers} / {selectedQuiz.questions.length}
+          </div>
+        </div>
+        <div className={styles.allQuestionsContainer}>
+          {selectedQuiz?.questions?.map((question, questionIndex) => (
+            <div key={questionIndex} className={styles.questionBlock}>
+              <h3>{`${question.id}. ${question.title}`}</h3>
+
+              {question.availableAnswers.map((answer, answerIndex) => (
+                <div key={answerIndex} className={styles.answersContainer}>
+                  <span>
+                    <ButtonAnswer
+                      className={`${buttonStyles.answerButton} ${
+                        selectedAnswers[questionIndex]?.[answerIndex] ||
+                        (selectedAnswers[questionIndex]?.correctIndex === answer
+                          ? buttonStyles.correctAnswer
+                          : "")
+                      }`}
+                      onClick={() =>
+                        handleAnswer(
+                          answer,
+                          questionIndex,
+                          answerIndex,
+                          question.correctAnswer
+                        )
+                      }
+                      disabled={
+                        Object.keys(selectedAnswers[questionIndex] || {})
+                          .length > 0
+                      }
+                    >
+                      {answer}
+                    </ButtonAnswer>
+                  </span>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     )
   );
