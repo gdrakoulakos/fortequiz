18d82d043da509a99fb60488df0c5a8394575c89
gdrakoulakos <georgediopan@gmail.com>
2025-03-10 15:33:21 +0200

    Remove comments

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,14 @@
 "use client";
 import { Nunito_Sans } from "next/font/google";
-import "./globals.css"; // Ensure global styles are imported
+import "./globals.css";
 import { AppProvider } from "@/context/AppContext";
 import Header from "@/components/organisms/Header/Header";
 import Footer from "@/components/organisms/Footer/Footer";
 
 const nunitoSans = Nunito_Sans({
-  weight: "300", // Light
-  subsets: ["latin"], // Ensure Latin characters are included
-  variable: "--font-nunito-sans", // Optional for CSS variable
+  weight: "300",
+  subsets: ["latin"],
+  variable: "--font-nunito-sans",
 });
 
 export default function RootLayout({ children }) {
