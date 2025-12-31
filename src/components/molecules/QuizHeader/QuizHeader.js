c06a84ddd8b5b3cca229cea6d858f7e8604da1a8
George Drakoulakos <g.drakoulakos@liquid.gr>
2025-12-31 21:20:49 +0200

    Replace the icon and add animation on correct answer icon

--- src/components/molecules/QuizHeader/QuizHeader.js
+++ src/components/molecules/QuizHeader/QuizHeader.js
@@ -1,5 +1,7 @@
 import styles from "./QuizHeader.module.css";
 import { QuizContext } from "@/context/AppContext";
+import Image from "next/image";
+import { motion } from "motion/react";
 
 export default function QuizHeader() {
   const { selectedQuiz, displayedQuestionIndex, clickedAnswersResults } =
@@ -10,17 +12,38 @@ export default function QuizHeader() {
   const quizQuestionCurrentNum = displayedQuestionIndex + 1;
   const clickedCorrectAnswers = clickedAnswersResults.correctAnswers;
 
+  const motionProps = {
+    initial: { opacity: 0, y: 30 },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: -30 },
+    transition: { duration: 0.2 },
+  };
+
   return (
     <div className={styles.quizHeader}>
       <p className={styles.questionCounter}>
         {quizQuestionCurrentNum}/{quizQuestionCounter}
       </p>
       <div className={styles.info}>
-        <p className={styles.category}>
-          {quizCategory} / {quizSubcategory}
-        </p>
+        <p className={styles.category}>{quizCategory}</p>
+        <p className={styles.subcategory}>{quizSubcategory}</p>
       </div>
-      <p className={styles.correctAnswers}>✅ {clickedCorrectAnswers}</p>
+      <div className={styles.correctInfoContainer}>
+        <Image
+          className={styles.correctIcon}
+          src={"/images/correct.png"}
+          alt="question image"
+          width={20}
+          height={20}
+        />
+        <motion.div
+          key={clickedCorrectAnswers}
+          className={styles.correctAnswers}
+          {...motionProps}
+        >
+          {clickedCorrectAnswers}
+        </motion.div>
+      </div>
     </div>
   );
 }
