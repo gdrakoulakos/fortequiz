ee9bcf39a9af2f4360dd295c2e8079503b516e3b
George Drakoulakos <g.drakoulakos@liquid.gr>
2026-02-08 23:46:19 +0200

    Remove unused code

--- src/app/myQuizzes/page.js
+++ src/app/myQuizzes/page.js
@@ -1,22 +1,7 @@
 "use client";
-import CardQuizzes from "@/components/organisms/CardQuizzes/CardQuizzes";
-import { QuizContext } from "../../context/AppContext";
+
 import { useEffect } from "react";
 
 export default function myQuizzes() {
-  const { allAthenaeumCourses, setCurrentInstitution } = QuizContext();
-
-  useEffect(() => {
-    setCurrentInstitution("authenaeum");
-  }, []);
-
-  return (
-    <div>
-      {allAthenaeumCourses.map((athenaeumCourse) => (
-        <div key={athenaeumCourse}>
-          <CardQuizzes group={athenaeumCourse} />
-        </div>
-      ))}
-    </div>
-  );
+  return <div></div>;
 }
